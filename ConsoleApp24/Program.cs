fd2e83f4fae4619b395618c883365364b93d1471
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-10 19:50:36 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        
+        public static double CalculateY()
+        {
+            
+            double y = (Math.Sqrt(7) + 7) / 3 + (Math.Sqrt(11) + 11) / 3 + (Math.Sqrt(15) + 15) / 3;
+            return y;
+        }
+
+        static void Main(string[] args)
+        {
+           
+            double result = CalculateY();
+            Console.WriteLine($"Значение y: {result}");
+            Console.ReadKey();
+        }
+    }
+}
